e48892b148e98c5ee18de38119cf10a59264e8ea
Redvan <redvanisation@gmail.com>
2019-12-04 04:01:21 -0500

    Add today's project

---
+++ 13.Slide-In-On-Scroll/script.js
@@ -0,0 +1,40 @@
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+const checkSlide = () => {
+  sliderImages.forEach(slideImg => {
+    // Half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - slideImg.height / 2;
+    
+    // bottom of the image
+    const imageBottom = slideImg.offsetTop + slideImg.height;
+
+    const halfWayShown = slideInAt > slideImg.offsetTop;
+    const notScrolledPast = window.scrollY < imageBottom;
+
+    if (halfWayShown && notScrolledPast) {
+      slideImg.classList.add('active');
+    } else {
+      slideImg.classList.remove('active');
+    }
+
+
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+// window.addEventListener('scroll', checkSlide);
