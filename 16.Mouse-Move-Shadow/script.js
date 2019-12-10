13d36979f4a7efee5482da123098246dc013610e
Redvan <redvanisation@gmail.com>
2019-12-10 04:52:36 -0500

    Adding a new project

---
+++ 16.Mouse-Move-Shadow/script.js
@@ -0,0 +1,24 @@
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+
+const walk = 100;
+
+function shadow(e) {
+    const { offsetWidth: width, offsetHeight: height } = hero;
+    let { offsetX: x, offsetY: y } = e;
+
+    if (this !== e.target) {
+        x += e.target.offsetLeft;
+        y += e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / width * walk) - (walk / 2));
+
+
+    text.style.textShadow = `
+        ${xWalk * -1}px ${yWalk * -1}px 10px rgba(0,0,0,.5)
+    `;
+}
+
+hero.addEventListener('mousemove', shadow);
