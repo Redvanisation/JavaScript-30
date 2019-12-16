4cbf1dba4bd84c64606eaff4926f88449ca50980
Redvan <redvanisation@gmail.com>
2019-12-16 05:20:40 -0500

    Adding a new project

---
+++ 20.Speech-Detection/script.js
@@ -0,0 +1,38 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+
+words.appendChild(p);
+
+
+recognition.addEventListener('result', (e) => {
+    console.log(e);
+
+    const transcript = Array.from(e.results)
+        .map(res => res[0])
+        .map(res => res.transcript)
+        .join('');
+
+    p.textContent = transcript;
+
+    if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+    }
+
+    if (transcript.includes('blabla')) {
+        console.log('blabla?');
+    }
+
+    console.log(transcript);
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
+
