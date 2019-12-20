9fe1ac1f763257f9a33b6c506babea5c2d02aae7
Redvan <redvanisation@gmail.com>
2019-12-20 04:28:09 -0500

    Adding fixed-nav project

---
+++ 24.Sticky-Nav/index.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector('#main');
+const navTop = nav.offsetTop;
+
+function fixNav() {
+  if (window.scrollY >= navTop) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+window.addEventListener('scroll', fixNav);
